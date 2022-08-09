002e9d09ff922879079ae1faee789f251caaa9fd
hasandgursoy <hasan.d.gursoy@gmail.com>
2022-08-09 13:48:57 +0300

    first commit

---
+++ 01-Variables/variables.go
@@ -0,0 +1,29 @@
+// Package Clause
+package main
+
+// Import Statement
+import "fmt"
+
+func main() {
+
+	var name string = "John"
+
+	// Variable Declaration
+
+	var surname string
+	surname = "Doe"
+
+	var isMarried bool = false
+
+	richEndex := 20000
+
+
+
+	fmt.Println("Hello", name)
+
+	fmt.Println(surname)
+
+	fmt.Println("is Married :",isMarried)
+
+	fmt.Println("What is the rich endex of joe :",richEndex)
+}
---
+++ 03-multiple_variables/multiple_variables.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+
+func main()  {
+	
+	var name,age,surname,weight,height,isLive = "hasan","23","gursoy",73,1.80,true
+
+	fmt.Println(name)
+	fmt.Println(surname)
+	fmt.Println(isLive)
+	fmt.Println(age)
+	fmt.Println(weight)
+	fmt.Println(height)
+
+	// Numeric data types default variable equal to ZERO
+
+	var finger int
+	fmt.Println(finger)
+
+
+}
